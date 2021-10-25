c83e7bacb45d97e77d39bd3d770b8a49b0c5f307
musasazib <musasazib@gmail.com>
2021-10-26 05:05:20 +0600

    Add pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,17 +9,23 @@ import { Link } from 'react-router-dom';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useCart(products);
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
     // products to be rendered on the UI
     const [displayProducts, setDisplayProducts] = useState([]);
+    const size = 10;
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
             .then(res => res.json())
             .then(data => {
-                setProducts(data);
-                setDisplayProducts(data);
+                setProducts(data.products);
+                setDisplayProducts(data.products);
+                const count = data.count;
+                const pageNumber = Math.ceil(count / 10);
+                setPageCount(pageNumber);
             });
-    }, []);
+    }, [page]);
 
 
 
@@ -67,6 +73,15 @@ const Shop = () => {
                         >
                         </Product>)
                     }
+                    <div className="pagination">
+                        {
+                            [...Array(pageCount).keys()].map(number => <button
+                                className={number === page ? 'selected' : ''}
+                                key={number}
+                                onClick={() => setPage(number)}
+                            >{number + 1}</button>)
+                        }
+                    </div>
                 </div>
                 <div className="cart-container">
                     <Cart cart={cart}>
