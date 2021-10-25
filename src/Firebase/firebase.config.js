2cfea2bbce7a40da56e2b7938e15b3e362f24610
musasazib <musasazib@gmail.com>
2021-10-26 03:32:56 +0600

    Add data from JSON to database and load on the UI

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,10 @@
-console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyCkPrMMTqVX_M16NUYoVco5iqz1e2H4GWM",
+    authDomain: "ema-john-shopping-site.firebaseapp.com",
+    projectId: "ema-john-shopping-site",
+    storageBucket: "ema-john-shopping-site.appspot.com",
+    messagingSenderId: "970639086036",
+    appId: "1:970639086036:web:9aca946c00213a70198641"
 };
-
+  
 export default firebaseConfig;
